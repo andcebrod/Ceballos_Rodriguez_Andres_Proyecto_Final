14b3c9f642285b1ddd840d6e8bfad4a74f60491c
andcebrod <acrmontellano@gmail.com>
2018-01-11 10:15:19 +0100

    11-05-2017

---
+++ Admin/admin.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Administracion</title>
+  </head>
+  <body>
+    <table>
+
+      <th>Tabla</th>
+      <th>Opciones a Realizar</th>
+      <tr>
+        <td>Usuarios</td>
+        <td>
+          <a href="/Usuarios/anadir_usuarios.php"><img src=""></a>
+          <a href="/Usuarios/editar_usuarios.php"><img src=""></a>
+          <a href="/Usuarios/eliminar_usuarios.php"><img src=""></a>
+        </td>
+      </tr>
+      <tr>
+        <td>Hermanos</td>
+        <td>
+            <a href="/Hermanos/anadir_hermanos.php"><img src=""></a>
+            <a href="/Hermanos/editar_hermanos.php"><img src=""></a>
+            <a href="/Hermanos/eliminar_hermanos.php"><img src=""></a>
+        </td>
+      </tr>
+      <tr>
+        <td>Articulos</td>
+        <td>
+          <a href="/Articulos/anadir_articulos.php"><img src=""></a>
+          <a href="/Articulos/editar_articulos.php"><img src=""></a>
+          <a href="/Articulos/eliminar_articulos.php"><img src=""></a>
+        </td>
+      </tr>
+      <tr>
+        <td>Eventos</td>
+        <td>
+          <a href="/Eventos/anadir_eventos.php"><img src=""></a>
+          <a href="/Eventos/editar_eventos.php"><img src=""></a>
+          <a href="/Eventos/eliminar_eventos.php"><img src=""></a>
+        </td>
+      </tr>
+      <tr>
+        <td>Bienes</td>
+        <td>
+          <a href="/Bienes/anadir_bienes.php"><img src=""></a>
+          <a href="/Bienes/editar_bienes.php"><img src=""></a>
+          <a href="/Bienes/eliminar_bienes.php"><img src=""></a>
+        </td>
+      </tr>
+    </table>
+
+  </body>
+</html>
