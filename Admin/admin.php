b93b2ccd581a7563969166fb89c879b1c12f36e6
andcebrod <acrmontellano@gmail.com>
2018-01-12 21:25:39 +0100

    12/1/2018

--- Admin/admin.php
+++ Admin/admin.php
@@ -6,47 +6,46 @@
   </head>
   <body>
     <table>
-
       <th>Tabla</th>
       <th>Opciones a Realizar</th>
       <tr>
         <td>Usuarios</td>
         <td>
-          <a href="/Usuarios/anadir_usuarios.php"><img src=""></a>
-          <a href="/Usuarios/editar_usuarios.php"><img src=""></a>
-          <a href="/Usuarios/eliminar_usuarios.php"><img src=""></a>
+          <a href="Usuarios/anadir_usuarios.php"><img src="Images/plus.png" height="12px" width="12px"></a>
+          <a href="Usuarios/editar_usuarios.php"><img src="Images/lapiz.png" height="12px" width="12px"></a>
+          <a href="Usuarios/eliminar_usuarios.php"><img src="Images/delete.png" height="12px" width="12px"></a>
         </td>
       </tr>
       <tr>
         <td>Hermanos</td>
         <td>
-            <a href="/Hermanos/anadir_hermanos.php"><img src=""></a>
-            <a href="/Hermanos/editar_hermanos.php"><img src=""></a>
-            <a href="/Hermanos/eliminar_hermanos.php"><img src=""></a>
+            <a href="Hermanos/anadir_hermanos.php"><img src="Images/plus.png" height="12px" width="12px"></a>
+            <a href="Hermanos/editar_hermanos.php"><img src="Images/lapiz.png" height="12px" width="12px"></a>
+            <a href="Hermanos/eliminar_hermanos.php"><img src="Images/delete.png" height="12px" width="12px"></a>
         </td>
       </tr>
       <tr>
         <td>Articulos</td>
         <td>
-          <a href="/Articulos/anadir_articulos.php"><img src=""></a>
-          <a href="/Articulos/editar_articulos.php"><img src=""></a>
-          <a href="/Articulos/eliminar_articulos.php"><img src=""></a>
+          <a href="Articulos/anadir_articulos.php"><img src="Images/plus.png" height="12px" width="12px"></a>
+          <a href="Articulos/editar_articulos.php"><img src="Images/lapiz.png" height="12px" width="12px"></a>
+          <a href="Articulos/eliminar_articulos.php"><img src="Images/delete.png" height="12px" width="12px"></a>
         </td>
       </tr>
       <tr>
         <td>Eventos</td>
         <td>
-          <a href="/Eventos/anadir_eventos.php"><img src=""></a>
-          <a href="/Eventos/editar_eventos.php"><img src=""></a>
-          <a href="/Eventos/eliminar_eventos.php"><img src=""></a>
+          <a href="Eventos/anadir_eventos.php"><img src="Images/plus.png" height="12px" width="12px"></a>
+          <a href="Eventos/editar_eventos.php"><img src="Images/lapiz.png" height="12px" width="12px"></a>
+          <a href="Eventos/eliminar_eventos.php"><img src="Images/delete.png" height="12px" width="12px"></a>
         </td>
       </tr>
       <tr>
         <td>Bienes</td>
         <td>
-          <a href="/Bienes/anadir_bienes.php"><img src=""></a>
-          <a href="/Bienes/editar_bienes.php"><img src=""></a>
-          <a href="/Bienes/eliminar_bienes.php"><img src=""></a>
+          <a href="Bienes/anadir_bienes.php"><img src="Images/plus.png" height="12px" width="12px"></a>
+          <a href="Bienes/editar_bienes.php"><img src="Images/lapiz.png" height="12px" width="12px"></a>
+          <a href="Bienes/eliminar_bienes.php"><img src="Images/delete.png" height="12px" width="12px"></a>
         </td>
       </tr>
     </table>
---
+++ indexse.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+<link rel="stylesheet" href="css/bootstrap.css">
+<link rel="stylesheet" href="css/styles.css">
+    <title>Gran Poder Montellano</title>
+  </head>
+  <body>
+
+  </body>
+</html>
