194886dc0b8e00a8592a1d174f030f1247e462b3
andcebrod <acrmontellano@gmail.com>
2018-01-14 16:37:53 +0100

    14/01/2018

---
+++ Admin/adminbueno.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+<link rel="stylesheet" href="css/bootstrap.css">
+<link rel="stylesheet" href="css/styles.css">
+    <title>Gran Poder Montellano</title>
+  </head>
+  <body>
+
+    <div class="container">
+
+      <div class="row" class="header">
+        <div class="col-md-12" id="social">
+          <div class="col-md-9" id="social1">
+            <span>Bienvenido a la web de la Hermandad Sacramental Ntro Padre</span>
+          </div>
+          <div class="col-md-3">
+            <div class="col-md-4">
+              <span>aa</span>
+            </div>
+            <div class="col-md-4">
+              <span>aa</span>
+            </div>
+            <div class="col-md-4">
+              <span>aa</span>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-12" id="logo">
+          <div class="col-md-3">
+            <span>aaaaa</span>
+          </div>
+          <div class="col-md-6">
+            <h2>Hermandad Sacramental Nuestro Padre Jesús del Gran Poder<br>
+            Maria Santisima de los Dolores <br>
+             y Beato Antonio Martinez Gil</h2>
+          </div>
+          <div class="col-md-3">
+            <span>aaaaaaa</span>
+          </div>
+        </div>
+      </div>
+      <div class="row" id="menu">
+        <div class="col-md-3" id="selmenu">
+          <span>Inicio</span>
+        </div>
+        <div class="col-md-3" id="selmenu">
+          <span>Eventos</span>
+        </div>
+        <div class="col-md-3" id="selmenu">
+          <span>Articulos</span>
+        </div>
+        <div class="col-md-3" id="selmenu">
+          <span>Bienes</span>
+        </div>
+
+
+
+      </div>
+
+      <div class="row" class="contenido">
+        <div class="col-md-9"  id="contprincipal">
+          <table class="table table-striped" id="administracion">
+            <thead>
+              <tr>
+                <th>Tabla</th>
+                <th>Opciones a Realizar</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td><span>Usuarios </span></td>
+                <td>
+                  <a href="Usuarios/anadir_usuarios.php"><img src="Images/plus.png" height="22px" width="22px"></a>
+                  <a href="Usuarios/editar_usuarios.php"><img src="Images/lapiz.png" height="22px" width="22px"></a>
+                  <a href="Usuarios/eliminar_usuarios.php"><img src="Images/delete.png" height="22px" width="22px"></a>
+                </td>
+              </tr>
+              <tr>
+                <td><span>Hermanos </span></td>
+                <td>
+                    <a href="Hermanos/anadir_hermanos.php"><img src="Images/plus.png" height="22px" width="22px"></a>
+                    <a href="Hermanos/editar_hermanos.php"><img src="Images/lapiz.png" height="22px" width="22px"></a>
+                    <a href="Hermanos/eliminar_hermanos.php"><img src="Images/delete.png" height="22px" width="22px"></a>
+                </td>
+              </tr>
+              <tr>
+                <td><span>Articulos </span></td>
+                <td>
+                  <a href="Articulos/anadir_articulos.php"><img src="Images/plus.png" height="22px" width="22px"></a>
+                  <a href="Articulos/editar_articulos.php"><img src="Images/lapiz.png" height="22px" width="22px"></a>
+                  <a href="Articulos/eliminar_articulos.php"><img src="Images/delete.png" height="22px" width="22px"></a>
+                </td>
+              </tr>
+              <tr>
+                <td><span>Eventos </span></td>
+                <td>
+                  <a href="Eventos/anadir_eventos.php"><img src="Images/plus.png" height="22px" width="22px"></a>
+                  <a href="Eventos/editar_eventos.php"><img src="Images/lapiz.png" height="22px" width="22px"></a>
+                  <a href="Eventos/eliminar_eventos.php"><img src="Images/delete.png" height="22px" width="22px"></a>
+                </td>
+              </tr>
+              <tr>
+                <td><span>Bienes </span></td>
+                <td>
+                  <a href="Bienes/anadir_bienes.php"><img src="Images/plus.png" height="22px" width="22px"></a>
+                  <a href="Bienes/editar_bienes.php"><img src="Images/lapiz.png" height="22px" width="22px"></a>
+                  <a href="Bienes/eliminar_bienes.php"><img src="Images/delete.png" height="22px" width="22px"></a>
+                </td>
+              </tr>
+
+
+            </tbody>
+          </table>
+
+        </div>
+        <div class="col-md-3" id="banners">
+          <div class="col-md-12">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt u
+              t labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamc
+              o laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+              in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cup
+              idatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+          </div>
+          <div class="col-md-12">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt u
+              t labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamc
+              o laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+              in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cup
+              idatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+          </div>
+
+        </div>
+
+      </div>
+
+      <div class="row" id="foot">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+           Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+           Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+           Excepteur sint occaecat cupidatat non proident,
+           sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+      </div>
+
+
+
+    </div>
+
+  </body>
+</html>
