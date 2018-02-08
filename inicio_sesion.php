3f45a7fec6f5c127e3716cfe1a30daa9b89e7631
andcebrod <acrmontellano@gmail.com>
2018-02-08 10:06:34 +0100

    8-2-2018

--- inicio_sesion.php
+++ inicio_sesion.php
@@ -6,10 +6,11 @@
   <head>
     <meta charset="utf-8">
 <link rel="stylesheet" href="css/bootstrap.css">
-<link rel="stylesheet" href="css/styles.css">
+<link rel="stylesheet" href="signin.css">
+<!-- <link rel="stylesheet" href="css/styles.css"> -->
     <title>Gran Poder Montellano</title>
   </head>
-  <body>
+  <body class="text-center">
     <?php
     //CREATING THE CONNECTION
       $connection = new mysqli("localhost", "root", "Admin2015", "granPoder",3316);
@@ -22,78 +23,21 @@
           exit();
       }
     ?>
-    <div class="container">
-      <div class="row" id="social">
-        <div class="col-md-9" id="social1">
-          <span>Bienvenido a la web de la Hermandad Sacramental Ntro Padre</span>
-        </div>
-        <div class="col-md-1">
-            <span>aa</span>
-          </div>
-        <div class="col-md-1">
-            <span>aa</span>
-          </div>
-        <div class="col-md-1">
-            <span>aa</span>
-          </div>
-      </div>
-      <div class="row header">
-        <div class="col-md-3">
-          <span>aaaaa</span>
-        </div>
-        <div class="col-md-6">
-          <h2>Hermandad Sacramental Nuestro Padre Jesús del Gran Poder<br>
-            Maria Santisima de los Dolores <br>
-             y Beato Antonio Martinez Gil</h2>
-        </div>
-        <div class="col-md-3">
-            <span>aaaaaaa</span>
-        </div>
 
-      </div>
-      <div class="row" id="menu">
-        <div class="col-md-2" id="selmenu">
-          <span><a href="index.php">Inicio</a></span>
-        </div>
-        <div class="col-md-2" id="selmenu">
-          <span><a href="Usuario/eventos.php">Eventos</a></span>
-        </div>
-        <div class="col-md-2" id="selmenu">
-          <span><a href="Usuario/articulos.php">Articulos</a></span>
-        </div>
-        <div class="col-md-2" id="selmenu">
-          <span><a href="Usuario/bienes.php">Bienes</a></span>
-        </div>
-        <div class="col-md-2" id="selmenu">
-          <span><a href="Usuario/registro.php">Registro</a></span>
-        </div>
-        <div class="col-md-2" id="selmenu">
-          <span><a href="Usuario/area_usuario.php">Área de Usuarios</a></span>
-        </div>
-
-
-
-      </div>
-
-      <div class="row" class="contenido">
-        <div class="col-md-9" id="contprincipal">
           <?php if (!isset($_POST["Correo"])) :?>
-            <form method="post">
-          <div class="form-group">
-            <label for="Correo">Correo: </label>
-            <input type="text" class="form-control" name="Correo" placeholder="Introduce correo">
+
+        <form class="form-signin" method="post">
+          <img class="mb-4" src="escudo.jpg" alt="" width="72" height="72">
+          <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+          <label for="inputEmail" class="sr-only">Email address</label>
+          <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="" name="Correo">
+          <label for="inputPassword" class="sr-only">Password</label>
+          <input type="password" class="form-control" placeholder="Password" required="" name="Pass">
           </div>
-          <div class="form-group">
-            <label for="pwd">Contraseña: </label>
-            <input type="password" class="form-control" placeholder="Introduce Contraseña" name="Pass">
-          </div>
-          <div class="form-group">
-            <input class="btn btn-default" type="submit" name="enviar" value="Iniciar Sesión">
-          </div>
-            </form>
-          <div class="form-group">
-            <label><b>¿Eres hermano? Regístrate gratis <a href="registro.php">Aquí</a>.</b></label>
-          </div>
+      <button class="btn btn-lg btn-primary btn-block" name="enviar" type="submit">Sign in</button>
+      <p class="mt-5 mb-3 text-muted">© 2017-2018</p>
+        </form>
+
         <?php else: ?>
 
           <?php
@@ -121,54 +65,9 @@
                 }
               }
 
-
-
            ?>
 
         <?php endif ?>
-
-
-
-
-
-        </div>
-        <div class="col-md-3" id="banners">
-          <div class="col-md-12">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt u
-              t labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamc
-              o laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
-              in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cup
-              idatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-          </div>
-          <div class="col-md-12">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt u
-              t labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamc
-              o laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
-              in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cup
-              idatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-          </div>
-
-        </div>
-
-      </div>
-
-      <div class="row" id="foot">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-           Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-           Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-           Excepteur sint occaecat cupidatat non proident,
-           sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-      </div>
-
-
-
-    </div>
 
   </body>
 </html>
